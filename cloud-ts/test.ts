17378ddb53110ea6d3e3ee3610823dd443288aa6
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2025-03-08 17:20:52 +0400

    remove extra files and clean code

--- cloud-ts/test.ts
+++
@@ -1,7 +0,0 @@
-// Simple test file
-console.log('This is a test file');
-
-// Export a simple function
-export function sayHello(name: string): string {
-  return `Hello, ${name}!`;
-} 
--- cloud/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHello = sayHello;
-// Simple test file
-console.log('This is a test file');
-// Export a simple function
-function sayHello(name) {
-    return `Hello, ${name}!`;
-}
