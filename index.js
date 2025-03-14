f1ba4d039a10c660a881357599070fb5339dccfd
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2025-03-15 03:38:06 +0400

    add needed configs

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const config = {
   liveQuery: {
     classNames: [], // List of classes to support for query subscriptions
   },
+  directAccess: true,
+  enforcePrivateUsers: true,
+  allowClientClassCreation: false,
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -50,7 +53,7 @@ if (!process.env.TESTING) {
   const port = process.env.PORT || 1337;
   const httpServer = http.createServer(app);
   httpServer.listen(port, function () {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('parse-server-example-ts running on port ' + port + '...');
   });
   // This will enable the Live Query real-time server
   ParseServer.createLiveQueryServer(httpServer);
